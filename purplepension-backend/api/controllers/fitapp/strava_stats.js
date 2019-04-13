37a9badcba5b6e1d418fbdb09d18579b25091851
Arnoud Bevers <a.bevers@jasbit.nl>
2019-04-13 21:16:21 +0200

    [FIX] Split fitapp controller into multiple functions

---
+++ purplepension-backend/api/controllers/fitapp/strava_stats.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+var request = require("request");
+const config = require("../../../data/strava_config");
+const HOST = "https://www.strava.com";
+const API = "/api/v3";
+const athleteId = 32222651;
+
+router.get("/", (req, res) => {
+  const uri = `${HOST}${API}/athletes/${athleteId}/stats`;
+  const args = {
+    headers: {
+      Authorization: "Bearer " + config.access_token,
+      "Content-Type": "application/json"
+    }
+  };
+  request.get(uri, args, function(err, response, body) {
+    if (!err) res.send(body);
+    else console.log(err);
+  });
+});
+
+module.exports = router;

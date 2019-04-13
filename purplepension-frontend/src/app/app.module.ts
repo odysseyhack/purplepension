01bb92e266c02f4f6a308fb506eb330977ac9fb4
Anouk Martens <anoukm013@live.nl>
2019-04-13 14:30:42 +0200

    Routing + Hamburger menu

--- purplepension-frontend/src/app/app.module.ts
+++ purplepension-frontend/src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { HomeComponent } from './home/home.component';
+import { InsightComponent } from './insight/insight.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { ConnectionsComponent } from './connections/connections.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    HomeComponent,
+    InsightComponent,
+    ForecastComponent,
+    ConnectionsComponent,
+    NotificationsComponent,
+    SettingsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,

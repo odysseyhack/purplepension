71de73a4d7900e408c42fcefaf1c9e81a4318a11
Luukvdm <luuk.vdm@hotmail.com>
2019-04-13 20:03:38 +0200

    Merge

--- purplepension-backend/api/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"),
-  app = express(),
-  router = express.Router(),
-  fitapp = require("./controllers/fitapp/fitapp_controller");
-
-router.use("/fitapp", fitapp);
-
-module.exports = router;

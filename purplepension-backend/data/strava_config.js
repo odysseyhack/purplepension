fae9bbb4b7f92ecae24345f3efca8a5801092641
Luukvdm <luuk.vdm@hotmail.com>
2019-04-14 10:57:27 +0200

    Updated token

--- purplepension-backend/data/strava_config.js
+++ purplepension-backend/data/strava_config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  access_token: "483549e509088d23f897a8797356fdbeaa6e50be ",
+  access_token: "0a86e7ab835cb9f64f5aad3df30850a2608f69b4  ",
   client_id: "26911",
   client_secret: "eda83725580450889c9ab09bbfa97ca2afc48a2b",
   redirect_uri: "localhost:3000/strava"
